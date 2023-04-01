3d7f9f1fb87f611b56d7904b1536d527e606bc93
Carter <carter@amp>
2023-04-01 14:37:55 -0600

    Cargo fmt

--- examples/request.rs
+++ examples/request.rs
@@ -1,6 +1,6 @@
 //! This example demonstrates making an xmlrpc request using the reqwest libary as a
 //! transport.
-//! 
+//!
 //! This example is intended to target a ROS1 master node, but is include more for
 //! illustrative purposes of showing how this library can be used than as a practical
 //! example.
@@ -13,14 +13,21 @@ fn main() {
     let body = serde_xmlrpc::request_to_string("getTopicTypes", vec![my_id.into()]).unwrap();
 
     // Send our request to the server and get a response back (using blocking API for simplicity)
-    let response = client.post(server_uri).body(body).send().unwrap().text().unwrap();
-    
+    let response = client
+        .post(server_uri)
+        .body(body)
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
+
     // Use this libaray to parse the values back out of the response
     // The ROS master server will give us 3 values back:
     // a status code to indicate success
     // an error message if anything went wrong
     // and a list of tuples of (topic name, topic data type)
-    let (_status_code, _error_msg, topics) = serde_xmlrpc::response_from_str::<(i8, String, Vec<(String, String)>)>(&response).unwrap();
+    let (_status_code, _error_msg, topics) =
+        serde_xmlrpc::response_from_str::<(i8, String, Vec<(String, String)>)>(&response).unwrap();
 
     println!("Ros reported the following registered topics and types: {topics:?}");
 
